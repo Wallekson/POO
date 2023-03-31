ec93eeaff400f910cb0eb9da91d4edd77674101b
Wallekson <walisoncastor@gmail.com>
2023-03-30 22:24:14 -0300

    Exercise Calculator

--- ExercicioPolimofismoESobrescrita/src/main/java/one/digitalinnovation/Main.java
+++ ExercicioPolimofismoESobrescrita/src/main/java/one/digitalinnovation/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         Funcionario gerente = new Gerente();  // Upcast
         Funcionario vendedor = new Vendedor(); // Upcast é implicito
-        Funcionario faxineiro = new Faxineiro(); // 
+        Funcionario faxineiro = new Faxineiro(); //
 
         // Gerente gerente = new Funcionario(); // nesse caso o DownCast não esta de forma explicita e por isso temos um erro
         Vendedor vendedor_ = (Vendedor) new Funcionario(); // DownCast explicito
